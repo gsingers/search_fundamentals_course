469d17e4451357733850e40239cf09f82bbc27bc
gsingers <gsingers@apache.org>
2022-05-01 16:16:39 -0400

    update gitpod command

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -1,11 +1,8 @@
 ln -s /workspace/kaggle /home/gitpod/.kaggle
 
 mkdir -p /workspace/opensearch
-mkdir -p /workspace/logstash
 mkdir -p /workspace/datasets
 sudo chown -R gitpod:gitpod /workspace/opensearch
-sudo  chown -R gitpod:gitpod /workspace/logstash
-ln -s /home/gitpod/logstash-7.13.2 /workspace/logstash/logstash-7.13.2
 
 
 cd docker
