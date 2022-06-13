22bf20d0e598159dcc87636c7f27d66adc6db22f
gsingers <gsingers@apache.org>
2022-06-13 11:22:06 -0400

    fix stylesheet call and add error handling on indexing

--- index-data.sh
+++ index-data.sh
@@ -31,9 +31,17 @@ mkdir $LOGS_DIR
 echo "Creating index settings and mappings"
 echo " Product file: $PRODUCTS_JSON_FILE"
 curl -k -X PUT -u admin  "https://localhost:9200/bbuy_products" -H 'Content-Type: application/json' -d "@$PRODUCTS_JSON_FILE"
+if [ $? -ne 0 ] ; then
+  echo "Failed to create index with settings of $PRODUCTS_JSON_FILE"
+  exit 2
+fi
 echo ""
 echo " Query file: $QUERIES_JSON_FILE"
 curl -k -X PUT -u admin  "https://localhost:9200/bbuy_queries" -H 'Content-Type: application/json' -d "@$QUERIES_JSON_FILE"
+if [ $? -ne 0 ] ; then
+  echo "Failed to create index with settings of $QUERIES_JSON_FILE"
+  exit 2
+fi
 
 cd $PYTHON_LOC
 echo ""
