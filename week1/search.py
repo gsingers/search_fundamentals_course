da2bf007b4fc138ec574a1225162d65a321f9f8c
Jose Montes de Oca <jose@newslit.co>
2022-10-09 18:51:44 -0400

    level 2 - query improvements

--- week1/search.py
+++ week1/search.py
@@ -111,17 +111,49 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
     query_obj = {
         'size': 10,
         "query": {
-            "bool": {
-                "must": [
+            "function_score": {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "query_string": {
+                                    "fields": [
+                                        "name^100", 
+                                        "shortDescription^50",
+                                        "longDescription^10"],
+                                    "query": user_query,
+                                    "phrase_slop": 3,
+                                }
+                            }
+                        ],
+                        "filter": filters
+                    }
+                },
+                "boost_mode": "replace",
+                "score_mode": "avg",
+                "functions": [
                     {
-                        "query_string": {
-                            "fields": ["name", "shortDescription", "longDescription"],
-                            "query": user_query,
-                            "phrase_slop": 3,
+                        "field_value_factor": {
+                            "field": "salesRankShortTerm",
+                            "modifier": "reciprocal",
+                            "missing": 100000000
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankMediumTerm",
+                            "modifier": "reciprocal",
+                            "missing": 100000000
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankLongTerm",
+                            "modifier": "reciprocal",
+                            "missing": 100000000
                         }
                     }
-                ],
-                "filter": filters
+                ]
             }
         },
         "aggs": {
