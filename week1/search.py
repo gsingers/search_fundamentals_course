59f04027bc09d636069e3cf248b998aa46c16847
Yigit Yesilpinar <yigityesilpinar@gmail.com>
2023-02-08 20:46:41 +0000

    added initial implementation of create_query with query, filter, sort, highlight and aggs

--- week1/search.py
+++ week1/search.py
@@ -94,7 +94,7 @@ def query():
     print("query obj: {}".format(query_obj))
 
     #### Step 4.b.ii
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
+    response = opensearch.search(body=query_obj, index="bbuy_products")
     # Postprocess results here if you so desire
 
     #print(response)
@@ -111,11 +111,68 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
     query_obj = {
         'size': 10,
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+            "bool": {
+                "must": [
+                    {
+                        "query_string": { 
+                            "query": user_query if user_query == '*' else f"\"{user_query}\"", 
+                            "fields": ["name^100", "shortDescription^50", "longDescription^10", "department"],
+                            "phrase_slop": 3
+                        }
+                    }
+                ],
+                "filter": filters if filters else [],
+            }
         },
-        "aggs": {
-            #### Step 4.b.i: create the appropriate query and aggregations here
+        "sort": [
+            {
+                f"{sort}": {
+                    "order": sortDir
+                }
+            }
+        ],
 
+        "highlight" : {
+            "pre_tags" : ["<mark>"],
+            "post_tags" : ["</mark>"],
+            "fields" : {
+                "name" : {},
+                "shortDescription":{},
+                "longDescription": {},
+                "department":{}
+            }
+        },
+        ### Step 4.b.i: create the appropriate query and aggregations here
+        "aggs": {
+            "department": {
+                "terms": {
+                    "field": "department.keyword",
+                    "min_doc_count": 1
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image",
+                }
+            },
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        {"key": "$", "from": 0, "to": 100},
+                        {"key": "$$", "from": 100, "to": 200},
+                        {"key": "$$$", "from": 200, "to": 300},
+                        {"key": "$$$$", "from": 300, "to": 400},
+                        {"key": "$$$$$", "from": 400, "to": 500},
+                        {"key": "$$$$$$", "from": 500},
+                    ]
+                },
+                "aggs": {
+                    "price_stats": {
+                        "stats": {"field": "regularPrice"}
+                    }
+                }
+            }
         }
     }
     return query_obj
