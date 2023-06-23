5722257142af6ede1b02b691dd2c0bd9e8def1a5
arijit bhattacharyya <arijitron@gmail.com>
2023-06-23 18:48:50 +0000

    token changed

--- install-kaggle-token.sh
+++ install-kaggle-token.sh
@@ -1,8 +1,8 @@
 
 read -p "Please enter your Kaggle API token here (e.g. {\"username\":\"corise\",\"key\":\"3536356fgdf\"}): " api_token
-echo "Your Kaggle API token is:" $api_token
+echo "Your Kaggle API token is:" $afa6976a75edba23c0f5946223eb54c1
 echo "Installing Kaggle API token to /home/gitpod/.kaggle/kaggle.json (soft-linked to /workspace/kaggle) ..."
-echo $api_token > /home/gitpod/.kaggle/kaggle.json
+echo $afa6976a75edba23c0f5946223eb54c1 > /home/gitpod/.kaggle/kaggle.json
 echo "Successfully installed."
 
 chmod 600 /home/gitpod/.kaggle/kaggle.json
