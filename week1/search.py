336d6a7db727f3df90b97decb60459bd40b047ea
adeyemi aladesawe <adeyemi.aladesawe@gmail.com>
2023-06-26 01:20:09 +0000

    Level 1 snapshot

--- week1/search.py
+++ week1/search.py
@@ -94,7 +94,7 @@ def query():
     print("query obj: {}".format(query_obj))
 
     #### Step 4.b.ii
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
+    response = opensearch.search(body=query_obj, index="bbuy_products")
     # Postprocess results here if you so desire
 
     #print(response)
@@ -111,22 +111,60 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
     query_obj = {
         'size': 10,
         "query": {
-            "match_all": {
-                "query_string": {
-                    "query": user_query
-                    "fields": ["name^100", "shortDescription^50", "longDescription^10"],
-                    "phrase_slop": 2, # check 3
-                    "filter": process_filters(filters),
-                }
-            } # Replace me with a query that both searches and filters
+            "bool": {
+                "must": [
+                   { 
+                        "query_string": {
+                            "query": user_query,
+                            "fields": ["name^100", "shortDescription^50", "longDescription^10"],
+                            "phrase_slop": 2, # check 3
+                        }
+                    }
+                ],
+                "filter": filters,
+            }# Replace me with a query that both searches and filters
         },
         "aggs": {
-            #### Step 4.b.i: create the appropriate query and aggregations here
+            #### Step 4.b.i: create the appropriate query and aggregations here\
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        { "key": "$", "to": 250 },
+                        { "key": "$$", "from": 250, "to": 500 },
+                        { "key": "$$$", "from": 500, "to": 750 },
+                        { "key": "$$$$", "from": 750, "to": 100 },
+                        { "key": "$$$$$", "from": 1000 },
+                    ]
+                },
+                "aggs": {
+                    "price_stats": {
+                        "stats": { "field": "regularPrice"}
+                    }
+                }
+            },
             "department": {
                 "terms": {
+                    "field": "department.keyword",
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image"
                 }
             }
-
-        }
+        },
+        "highlight": {
+            "fields": {
+                "name": {},
+                "shortDescription": {},
+                "longDescription": {}
+            }
+        },
+        "sort": [
+            { "regularPrice": { "order": sortDir } },
+            { "name.keyword": { "order": sortDir } },
+            { sort: { "order": sortDir } }
+        ]
     }
     return query_obj
