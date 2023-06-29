60f081df596be4d056fb2efa6e6e13ad1e2053dd
Rukayat Sadiq <fruqoyya@gmail.com>
2023-06-29 23:10:00 +0000

    build query with function scoring

--- week1/search.py
+++ week1/search.py
@@ -111,15 +111,44 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
     query_obj = {
         'size': 10,
         "query": {
-            "bool": {
-                "must":  {
-                    "query_string": {
-                        "query": user_query,
-                        "fields": ["name", "shortDescription", "longDescription"],
-                        "phrase_slop": 3
+            "function_score": {
+                "query": {
+                    "bool": {
+                        "must":  [{
+                            "query_string": {
+                                "query": user_query,
+                                "fields": ["name^100", "shortDescription^50", "longDescription^10", "department"],
+                                "phrase_slop": 3
+                            }
+                        }],
+                        "filter": filters
                     }
                 },
-                "filter": filters
+                "boost_mode": "replace",
+                "score_mode": "avg",
+                "functions": [
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankLongTerm",
+                            "missing": 100000000,
+                            "modifier": "reciprocal"
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankMediumTerm",
+                            "missing": 100000000,
+                            "modifier": "reciprocal"
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankShortTerm",
+                            "missing": 100000000,
+                            "modifier": "reciprocal"
+                        }
+                    }
+                ]
             }
         },
         "aggs": {
