e0842c55d42fb2b3632cdd65b30d83f0eafe6ec6
Sanjay Menon <sanjay.menon@gmail.com>
2023-07-03 13:17:16 +0000

    suggester code

--- week2/utilities/query_utils.py
+++ week2/utilities/query_utils.py
@@ -160,15 +160,31 @@ def create_query(user_query, filters, sort="_score", sortDir="desc", size=10, in
 def add_spelling_suggestions(query_obj, user_query):
     #### W2, L2, S1
     print("TODO: IMPLEMENT ME")
-    #query_obj["suggest"] = {
-    #    "text": user_query,
-    #    "phrase_suggest": {
-
-    #    },
-    #    "term_suggest": {
-
-    #    }
-    #}
+    query_obj["suggest"] = {
+        "text": user_query,
+        "phrase_suggest": {
+            "phrase":{
+                "field" : "suggest.trigrams",
+                "direct_generator" : [ {
+                    "field" : "suggest.trigrams",
+                    "suggest_mode" : "popular",
+                    "min_word_length" :  2
+                    } ],
+                "highlight" : {
+                    "pre_tags": ["<b>"], 
+                    "post_tags": ["</b>"], 
+                    "fields" : {"name"}
+                }
+            }
+        },
+        "term_suggest": {
+            "term":{
+                "field":"suggest.text",
+                "min_word_length": 3,
+                "suggest_mode": "popular"
+            }
+        }
+    }
 
 
 # Given the user query from the UI, the query object we've built so far and a Pandas data GroupBy data frame,
